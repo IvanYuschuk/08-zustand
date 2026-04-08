c89d5dba50f4a40c1e9cef10397c50df6926c19f
IvanYuschuk <ivan.yuschuk77@gmail.com>
2026-04-08 14:29:56 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NoteHub",
+  description: " NoteHub is a simple and efficient application designed for managing personal notes. It helps keep your thoughts organized and accessible in one place, whether you are at home or on the go.",
+};
+
+export default function RootLayout({
+  children,
+  modal, 
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+        <TanStackProvider >
+          <body>
+            <Header />
+              {children}
+              {modal}
+            <Footer />
+          </body>
+         </TanStackProvider>
+    </html>
+  );
+}
